bcf65f3f7eb7999c1ab6d41d0ef470b589b6bdaa
Josias Sekhebesa <68565393+JosiasSekhebesa@users.noreply.github.com>
2020-08-06 18:28:52 +0200

    PEP8 compliance for all functions.
    Updated the file to comply to PEP8 standards.

--- mymodule.py
+++ mymodule.py
@@ -68,21 +68,28 @@ stop_words_dict = {
     ]
 }
 
-#function 1: 
+# function 1:
+
 
 def dictionary_of_metrics(items):
     """
     Function should allow a list as input:
 
     Returns:
-    It should return a dict with keys 'mean', 'median', 'std', 'var', 'min', and 'max',
-    corresponding to the input list, respectively.
+    It should return a dict with keys 'mean', 'median', 'std', 'var', 'min',
+    and 'max',corresponding to the input list, respectively.
     """
-    my_dict = {'mean':round(np.mean(items),2),'median':round(np.median(items),2),'var':round(np.var(items,ddof=1),2),'std':round(np.std(items,ddof=1),2),'min':round(np.min(items),2),'max':round(np.max(items),2)}
+    my_dict = {'mean': round(np.mean(items), 2),
+               'median': round(np.median(items), 2),
+               'var': round(np.var(items, ddof=1), 2),
+               'std': round(np.std(items, ddof=1), 2),
+               'min': round(np.min(items), 2),
+               'max': round(np.max(items), 2)}
+
     return my_dict
 
+# function 2:
 
-#function 2:
 
 def five_num_summary(items):
     """
@@ -100,17 +107,16 @@ def five_num_summary(items):
         five_num_summary(welkom)
         >>> {'max': 7, 'median': 4.0, 'min': 1, 'q1': 2.5, 'q3': 5.5}
     """"
-    # Input for the funtion
     summary = {'max': round(np.max(items), 2),
                'median': round(np.median(items), 2),
                'min': round(np.min(items), 2),
                'q1': round(np.percentile(items, 25, axis=0), 2),
                'q3': round(np.percentile(items, 75, axis=0), 2)}
 
-    # return the five number summary
     return summary
 
-#function 3: 
+# function 3:
+
 
 def date_parser(dates):
     """
@@ -118,105 +124,120 @@ def date_parser(dates):
     Each string in the input list is formatted as 'yyyy-mm-dd hh:mm:ss'.
 
     Returns:
-    The function should return a list of strings 
-    where each element in the returned list contains only the date in the 'yyyy-mm-dd' format.
-    
+    The function should return a list of strings
+    where each element in the returned list contains only the date in the
+    'yyyy-mm-dd' format.
+
     For Example:
-                dates[:3] == [ '2015-12-25 11:45:54', '2015-12-25 11:40:03', 2015-12-25 11:30:02']
+    dates[:3] == [ '2015-12-25 11:45:54', '2015-12-25 11:40:03',
+                    2015-12-25 11:30:02']
 
     Output:
            dates[:3] == ['2015-12-25', '2015-12-25', '2015-12-25' ]
     """
-    # your code here
-    
     for i in dates:
-        i = pd.to_datetime(dates, format = '%Y-%m-%d').strftime('%Y-%m-%d')
-        
+        i = pd.to_datetime(dates, format='%Y-%m-%d').strftime('%Y-%m-%d')
+
     return list(i)
 
-#function 4: 
+# function 4:
+
 
 def extract_municipality_hashtags(df):
     """
     This function takes in a pandas dataframe:
 
     Returns:
-    A modified dataframe that includes two new columns that contain information 
+    A modified dataframe that includes two new columns that contain information
     about the municipality and hashtag of the tweet
     """
     municipality = []
-    
+
     for i in range(0, len(df)):
         city_found = ''
+
         for x, y in mun_dict.items():
             v = df.iloc[i][0].find(x)
             if v is not -1:
                 city_found = y
+
             else:
                 city_found = np.nan
         municipality.append(city_found)
+
         df1 = pd.DataFrame(municipality, columns=['municipality'])
+
         df2 = df.join(df1, lsuffix='Date', rsuffix='municipality')
-        H = [list(filter(lambda x: x.startswith('#'), df['Tweets'][i].lower().split())) for i in range(len(df.index.values))]
-        hashtags = pd.DataFrame([np.nan if x==[] else x for x in H], columns=['hashtags'])
+
+        H = [list(filter(lambda x: x.startswith('#'),
+                  df['Tweets'][i].lower().split()))
+             for i in range(len(df.index.values))]
+        hashtags = pd.DataFrame([np.nan if x == [] else x for x in H],
+                                columns=['hashtags'])
+
         final = df2.join(hashtags, lsuffix='municipality', rsuffix='hashtags')
+
     return final
 
+# function 5
 
-#function 5
 
 def number_of_tweets_per_day(df):
-    """ 
+    """
     This function returns the number of tweets per day:
-    ---
+
     Parameters: items- dataframe
-    
-    Return: num_of_tweets_per_day: 
+
+    Return: num_of_tweets_per_day:
     prints Date: number of tweets
-    ---
+
     """
     df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
-    
+
     result = df[['Tweets']].groupby(df['Date'].dt.date).count()
-    
-    return result 
-    
 
+    return result
 
-#function 6:
+# function 6:
+
 
 def word_splitter(df):
     """
-    This function splits the sentences in a dataframe's column into a list of the separate words:
+    This function splits the sentences in a dataframe's column into a list of
+    the separate words:
 
     Returns:
-    The function should split the sentences in the 'Tweets' into a list of seperate words, 
-    and place the result into a new column named 'Split Tweets'. The resulting words must all be lowercase!
+    The function should split the sentences in the 'Tweets' into a list of
+    seperate words,
+    and place the result into a new column named 'Split Tweets'. The resulting
+    words must all be lowercase!
     """
-    ### START FUNCTION
-    
-    words= df['Tweets']
-    
-    df['Split Tweets'] = [ word.lower().split() for word in words]
-    
+    words = df['Tweets']
+
+    df['Split Tweets'] = [word.lower().split() for word in words]
+
     return df
 
-### END FUNCTION
-  
+# function 7:
 
-
-#function 7:
 
 def stop_words_remover(df):
     """
     This function removes english stop words from a tweet:
 
-    Returns: 
-    Provides a modified dataframe that removes all stop words in the tokenised list.
-    The resulting tokenised list should be placed in a column named `"Without Stop Words"`.
+    Returns:
+    Provides a modified dataframe that removes all stop words in the tokenised
+    list.
+    The resulting tokenised list should be placed in a column named
+    `"Without Stop Words"`.
     """
     stop_words = stop_words_dict["stopwords"]
-    df["Without Stop Words"] = df["Tweets"].str.lower().str.split()
-    df["Without Stop Words"] = df["Without Stop Words"].apply(lambda x: [word for word in x if word not in stop_words])
-    return df
 
+    df["Without Stop Words"] = df["Tweets"].str.lower().str.split()
+
+    df["Without Stop Words"] = df["Without Stop Words"].apply(lambda x:
+                                                              [word for word in
+                                                               x if word not in
+                                                               stop_words])
+
+    return df
